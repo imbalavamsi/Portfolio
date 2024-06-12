d105d2d21824ec061575bdfdf42742443ae6373a
Bala Vamsi Maragani <mbalavamsi26@gmail.com>
2024-06-12 18:21:24 -0400

    EmailJS integration
    Form submission fixed

--- script.js
+++ script.js
@@ -18,9 +18,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const contact = event.target.contact.value;
     const message = event.target.message.value;
 
-    // Send the form data to your email using a server-side script or service like EmailJS
-    // Example using EmailJS:
-    emailjs.send('service_id', 'template_id', {
+    emailjs.send('service_loo7p3c', 'template_j3q2s15', {
         from_name: name,
         from_email: email,
         contact_number: contact,
